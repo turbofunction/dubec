dc5f87d19018f37d5ae488b33fe7c37138d988b6
Janne Savukoski <janne@savukoski.name>
2014-12-12 01:07:50 +0200

    zip script tweak

--- mkzip.sh
+++ mkzip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -f DUBEC-quickteck.zip
+rm -f DUBEC.zip
 
 zip DUBEC-quickteck.zip DUBEC.cmp DUBEC.crc DUBEC.drd DUBEC.drl DUBEC.l15 DUBEC.ly2 DUBEC.plc DUBEC.pls DUBEC.sol DUBEC.stc DUBEC.sts README.txt
