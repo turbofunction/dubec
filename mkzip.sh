cf20c71d86b37eb3db5d61cff3de2b78c48b0fc6
Janne Savukoski <janne@savukoski.name>
2014-12-12 15:34:55 +0200

    Missed occurrence

--- mkzip.sh
+++ mkzip.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-rm -f DUBEC.zip
+zip_file = "DUBECv1_1.zip"
 
-zip DUBEC-quickteck.zip DUBEC.cmp DUBEC.crc DUBEC.drd DUBEC.drl DUBEC.l15 DUBEC.ly2 DUBEC.plc DUBEC.pls DUBEC.sol DUBEC.stc DUBEC.sts README.txt
+rm -f $zip_file
+
+zip $zip_file DUBEC.cmp DUBEC.crc DUBEC.drd DUBEC.drl DUBEC.l15 DUBEC.ly2 DUBEC.plc DUBEC.pls DUBEC.sol DUBEC.stc DUBEC.sts README.txt
